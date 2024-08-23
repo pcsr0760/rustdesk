4d5c621dac065d954d4c1d018b3dd18a4720315f
pcsr0760 <82314780+pcsr0760@users.noreply.github.com>
2024-08-23 16:39:51 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rd.wooomooo.com"];
-pub const PUBLIC_RS_PUB_KEY: &str = "JJIiSb5HwF3kLZvQoo8gt3Lfgft3OKcV2fOU=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["rd.570011.xyz"];
+pub const PUBLIC_RS_PUB_KEY: &str = "VbxKqQKaczpMrq7RnvVLPofwgiQminv1KuYTgW4XkPA=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
