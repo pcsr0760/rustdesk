25b49305f257e544f77cd816924f1c661029903b
pcsr0760 <82314780+pcsr0760@users.noreply.github.com>
2024-08-31 20:57:30 +0800

    common.rs

--- src/common.rs
+++ src/common.rs
@@ -894,7 +894,7 @@ pub fn get_api_server(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://rd.570011.xyz:21114".to_owned()
+    "http://rd.570011.xyz:21114".to_owned()
 }
 
 pub fn get_audit_server(api: String, custom: String, typ: String) -> String {
